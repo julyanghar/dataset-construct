e4ce2bf5bea1388ca31afe9c50d091a7759c4078
julyanghar <julyang5216@gmail.com>
2025-10-27 15:15:16 -0500

    mdpo

--- generate_text/run.sh
+++ generate_text/run.sh
@@ -4,9 +4,6 @@ image_folder="/home/yilin/dataset/train2014/"
 data_path="/home/yilin/dataset-construct/preference_data/yilin_pref_data_pooler_output.json"
 # data_path="/home/yilin/dataset-construct/preference_data/pref_data.json"
 
-
-export OPENAI_API_KEY="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"
-
 # python -m debugpy --connect 5679 new_unpreferred_response.py \
 python new_unpreferred_response.py \
     --data_path $data_path \
